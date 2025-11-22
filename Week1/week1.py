897ccdb54faceeaca52f3609d98c5d163534823a
ZawGyi2022 <75303929+ZawMT@users.noreply.github.com>
2025-11-23 08:44:39 +1300

    Create week1.py

---
+++ Week1/week1.py
@@ -0,0 +1,14 @@
+Name = "John Doe"
+Age = 28
+Skills = ["Python", "SQL", "Power BI"]
+Education = ("BSc Computer Science", 2020)
+ContactDetails = {"email": "johndoe@jd.com", "phone": "022 123 456"}
+Certifications = {"Azure", "AWS", "Azure"}
+print("Component           |   Data Type   |   Example")
+print("-----------------------------------------------")
+print(f"Name                |   String      |   {Name}")
+print(f"Age                 |   Integer     |   {Age}")
+print(f"Skills              |   List        |   {Skills}")
+print(f"Education           |   Tuple       |   {Education}")
+print(f"Contact Details     |   Dictionary  |   {ContactDetails}")
+print(f"Certifications      |   Set         |   {Certifications}")
