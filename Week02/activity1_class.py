6fee0096088cf83ddd1f5bd72b61ccaebcc1120c
ZawGyi2022 <75303929+ZawMT@users.noreply.github.com>
2025-11-30 09:23:52 +1300

    Using class

---
+++ Week02/activity1_class.py
@@ -0,0 +1,43 @@
+class TestMath:
+    def __init__(self, n):
+        self.num = n
+        self.kept_num = n
+
+    def reset(self):
+        self.num = self.kept_num
+    
+    def get_factorial(self):
+        if self.num > 1:
+            self.num = self.num - 1
+            return self.num * self.get_factorial()    
+    
+        return 1
+    
+    def get_fibonacci(self, lst_resulted = None):
+        if lst_resulted is None:
+            self.reset()
+            lst_resulted = self.num * [0]
+            
+        if self.num <= 0:
+            self.num = self.num - 1
+            return lst_resulted
+        elif self.num == 1:
+            self.num = self.num - 1
+            lst_resulted[0] = 0
+            return lst_resulted
+        elif self.num == 2:
+            self.num = self.num - 1
+            lst_resulted[0], lst_resulted[1] = 0, 1
+            return lst_resulted 
+        else:
+            if lst_resulted[self.num-1] == 0:
+                self.get_fibonacci(lst_resulted)
+                self.num = self.num - 1
+                lst_resulted[self.num-1] = lst_resulted[self.num-2] + lst_resulted[self.num-3]
+            return lst_resulted 
+
+
+num = int(input("Give a number:"))
+test_math = TestMath(num)
+print(f"Factorial: {test_math.get_factorial()}")
+print(f"Fibonacci: {test_math.get_fibonacci()}")
---
+++ Week02/in_class.py
@@ -0,0 +1,7 @@
+
+def square(x):
+    return x*x
+print("Testing")
+print(round(45.6))
+print(round(4.4) + 3)
+print(square(2+3*5))
