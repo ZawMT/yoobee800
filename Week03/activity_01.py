a296757651156be297771ee0d07bf5f5de169240
ZawGyi2022 <75303929+ZawMT@users.noreply.github.com>
2025-12-06 08:49:08 +1300

    Counting a char

--- Week03/activity_01.py
+++ Week03/activity_01.py
@@ -2,7 +2,7 @@ import os
 
 class FileTest:
     def __init__(self):
-        pass
+        self.no_of_searching_char = 0
     
 
     def open_n_print(self, filename):
@@ -10,6 +10,7 @@ class FileTest:
             self.convert_line_endings(filename) #Need to do this because file contents need to be adjusted to read in Mac
             with open(filename, 'r', encoding='utf-8-sig') as data:  #'utf-8-sig' handles BOM
                 for line in data:
+                    self.count_char(line, '*')
                     print(line.rstrip('\r\n'))  #Strip all line endings
         except Exception as x:
             print(f'Error in open_n_print: {x}')
@@ -25,11 +26,15 @@ class FileTest:
         except Exception as x:
             print(f'Error in convert_line_endings: {x}')
 
+    
+    def count_char(self, line, char_to_count):
+        self.no_of_searching_char = self.no_of_searching_char + line.count(char_to_count)
             
 def main():
     current_working_directory = os.getcwd()
     file_test = FileTest()
     file_test.open_n_print(current_working_directory + "/demo_file.txt")
+    print(f"No. of * in this text: {file_test.no_of_searching_char}")
 
 
 if __name__ == "__main__":
