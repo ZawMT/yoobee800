dabfa9af8e048fa4d8315bb3171ffeef20f6ee5f
ZawGyi2022 <75303929+ZawMT@users.noreply.github.com>
2025-12-06 08:39:54 +1300

    Open and read a file

---
+++ Week03/activity_01.py
@@ -0,0 +1,36 @@
+import os
+
+class FileTest:
+    def __init__(self):
+        pass
+    
+
+    def open_n_print(self, filename):
+        try:
+            self.convert_line_endings(filename) #Need to do this because file contents need to be adjusted to read in Mac
+            with open(filename, 'r', encoding='utf-8-sig') as data:  #'utf-8-sig' handles BOM
+                for line in data:
+                    print(line.rstrip('\r\n'))  #Strip all line endings
+        except Exception as x:
+            print(f'Error in open_n_print: {x}')
+
+
+    def convert_line_endings(self, filename):
+        try:
+            with open(filename, 'r', encoding='utf-8-sig') as data:
+                lines = data.readlines()
+            with open(filename, 'w', encoding='utf-8') as data:
+                data.writelines([line.rstrip('\r\n') + '\n' for line in lines])
+            print("Line endings converted to Unix style.")
+        except Exception as x:
+            print(f'Error in convert_line_endings: {x}')
+
+            
+def main():
+    current_working_directory = os.getcwd()
+    file_test = FileTest()
+    file_test.open_n_print(current_working_directory + "/demo_file.txt")
+
+
+if __name__ == "__main__":
+    main()
